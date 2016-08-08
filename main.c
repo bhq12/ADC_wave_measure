aaac05b9e2f0242701bf9c87f5c0914e8dd5a7f2
bhq12 <bhq12@uclive.ac.nz>
2016-08-08 15:05:40 +1200

    move ADC init to task file from main, make ADC reading array non-static

--- main.c
+++ main.c
@@ -74,7 +74,7 @@ int main( void )
 	SysCtlClockSet( SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ );
 
 	xADCQueue = xQueueCreate(1000, sizeof (unsigned long));
-	initialiseADC();
+
 
 
 //	ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
@@ -142,50 +142,5 @@ void vApplicationTickHook( void )
 }
 
 
-//*****************************************************************************
-//
-//  Function to initialise the ADC and associated pins/peripherals
-//
-//*****************************************************************************
-void initialiseADC(void){
-  // The ADC0 peripheral must be enabled for use.
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 
-  //
-  // For this example ADC0 is used with AIN0 on port B1. This
-  // was given by the LM3S1968 data sheet.
-  // Therefore, GPIO port B needs to be enabled
-  // so these pins can be used.
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-
-  // Select the analog ADC function for these pins.
-  // The LM3S1968 data sheet was consulted to see which functions are
-  // allocated per pin.
-  GPIOPinTypeADC(GPIO_PORTB_BASE, GPIO_PIN_1);
-
-  // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
-  // will do a single sample when the processor sends a signal to start the
-  // conversion.  Each ADC module has 4 programmable sequences, sequence 0
-  // to sequence 3.  This example is arbitrarily using sequence 3.
-  ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
-
-  // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
-  // single-ended mode (default) and configure the interrupt flag
-  // (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
-  // that this is the last conversion on sequence 3 (ADC_CTL_END).  Sequence
-  // 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
-  // sequence 0 has 8 programmable steps.  Since we are only doing a single
-  // conversion using sequence 3 we will only configure step 0.  For more
-  // information on the ADC sequences and steps, reference the datasheet.
-  ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
-                             ADC_CTL_END);
-
-  // Since sample sequence 3 is now configured, it must be enabled.
-  ADCSequenceEnable(ADC0_BASE, 3);
-
-  // Clear the interrupt status flag.  This is done to make sure the
-  // interrupt flag is cleared before we sample.
-  ADCIntClear(ADC0_BASE, 3);
-
-}
 
--- screenDrawTask.c
+++ screenDrawTask.c
@@ -9,7 +9,7 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 extern xQueueHandle xADCQueue;
-static unsigned long sample[1];
+
 
 void screenDrawTask( )
 {
@@ -101,18 +101,57 @@ void makeNoiseTask( void *pvParameters )
 	}
 }
 
+//*****************************************************************************
+//
+//  Function to initialise the ADC and associated pins/peripherals
+//
+//*****************************************************************************
+void initialiseADC(void){
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	GPIOPinTypeADC(GPIO_PORTA_BASE, GPIO_PIN_1); //ADC1
+	GPIOPinTypeADC(GPIO_PORTB_BASE, GPIO_PIN_1); //ADC0
+
+	//Set up ADC0 to use sequence number 2. This means that
+	//
+	//ADC_TRIGGER_PROCESSOR means that we can cause the processor
+	//to trigger an ADC sample, which allows us to later read a sample
+	ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR, 0);
+
+
+	//Configure a step of the sample sequencer.
+	//In this case, we are configuring ADC0 to use sequence number 2,
+	//and we are configuring step 0. Step 0 will use input channel 0
+	//ADCSequenceStepConfigure(ADC0_BASE, 2, 0, ADC_CTL_CH0 | ADC_CTL_IE | ADC_CTL_END);
+	ADCSequenceStepConfigure(ADC0_BASE, 2, 0, ADC_CTL_CH0);
+
+	ADCSequenceStepConfigure(ADC0_BASE, 2, 1, ADC_CTL_CH1 | ADC_CTL_IE | ADC_CTL_END);
+	//Enable sample sequence 2
+	ADCSequenceEnable(ADC0_BASE, 2);
+
+	//Clear the source of a sample sequence interrupt
+	//This means that the interrupt no longer asserts
+	ADCIntClear(ADC0_BASE, 2);
+}
+
 void pollADCTask(){
 
-	sample[0] = 0ul;
+	initialiseADC();
+	unsigned long sample[4];
+	sample[0] = 108ul;
 	while(true){
 
-		ADCProcessorTrigger(ADC0_BASE, 3);
+		//Triggers an ADC sampling, using sequence number 2
+		ADCProcessorTrigger(ADC0_BASE, 2);
 
-		while(!ADCIntStatus(ADC0_BASE, 3, false))
-		{
+		//Wait for the sampling to finish
+		while (!ADCIntStatus(ADC0_BASE, 2, false)) {
 		}
-		ADCSequenceDataGet(ADC0_BASE, 0, sample);
-		unsigned long value = sample[0];
+
+		//Obtain the sample
+		ADCSequenceDataGet(ADC0_BASE, 2, sample);
+		unsigned long value = (unsigned long)sample[0];
 		xQueueSend(xADCQueue, &value, 10);
 	}
 }
