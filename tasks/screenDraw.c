afe6e166b6f2f069c409564d1519e11e3f7663a9
bhq12 <bhq12@uclive.ac.nz>
2016-08-13 21:31:48 +1200

    add nice looking interface for screen

--- tasks/screenDraw.c
+++ tasks/screenDraw.c
@@ -13,6 +13,7 @@ extern xQueueHandle xScreenStateQueue;
 void screenDrawTask( )
 {
 	RIT128x96x4Init(1000000);
+	printTitles();
 
 	int state = 1;
 	unsigned long adcVal;
@@ -28,7 +29,9 @@ void screenDrawTask( )
 			queue = &xADCQueue0;
 		}
 		adcVal = receiveFromQueue(*queue);
+
 		printStatus(state, adcVal);
+
 	}
 }
 
@@ -59,10 +62,86 @@ unsigned long receiveFromQueue(xQueueHandle queue){
 }
 
 void printStatus(int state, unsigned long adcVal){
-	char adcMessage [25];
 
-	sprintf (adcMessage, "ADC%d val: %lu   ", state, adcVal);
+
+	float frequency = 22;
+	float period = 0.02;
+	float amplitude = 0.8;
+	float dutyCycle = 24.5;
+
+	if(state){
+		//square wave/adc1: frequency, period, duty cycle
+		RIT128x96x4StringDraw("ADC1: square wave", 10, 25, 'm');
+		printDutyCycle(dutyCycle);
+	}
+	else{
+		//sine wave/adc0: frequency, period, amplitude
+		RIT128x96x4StringDraw("ADC0: sine wave  ", 10, 25, 'm');
+		printAmplitude(amplitude);
+	}
+
+	//printFrequency(frequency);
+	//printPeriod(period);
+	printADC(state, adcVal);
+
+}
+
+void printADC(int state, unsigned long adcVal){
+	char adcMessage [25];
+	snprintf (adcMessage, sizeof(adcMessage), "ADC%d val:", state);
+
 	//IntMasterDisable();
-	RIT128x96x4StringDraw(adcMessage, 10, 10, 'm');
+	RIT128x96x4StringDraw(adcMessage, 0, 50, 'm');
 	//IntMasterEnable();
+	snprintf (adcMessage, sizeof(adcMessage), "%lu", adcVal);
+
+	//IntMasterDisable();
+	RIT128x96x4StringDraw(adcMessage, 75, 50, 'm');
+}
+
+void printFrequency(float frequency){
+	char message [8];
+
+	snprintf (message, sizeof(message), "%.2fkHz", frequency);
+	//IntMasterDisable();
+	RIT128x96x4StringDraw(message, 75, 60, 'm');
+	//IntMasterEnable();
+}
+
+void printPeriod(float period){
+	char message [8];
+
+	snprintf (message, sizeof(message), "%.2fms", period);
+	//IntMasterDisable();
+	RIT128x96x4StringDraw(message, 75, 70, 'm');
+	//IntMasterEnable();
+}
+void printAmplitude(float amplitude){
+
+	//IntMasterDisable();
+	RIT128x96x4StringDraw("Amplitude:", 0, 80, 'm');
+	//IntMasterEnable();
+	//char message [8];
+
+	//snprintf (message, sizeof(message), "%.2fV", amplitude);
+	//RIT128x96x4StringDraw(message, 75, 80, 'm');
+}
+
+void printDutyCycle(float dutyCycle){
+
+	//IntMasterDisable();
+	RIT128x96x4StringDraw("Duty Cycle:", 0, 80, 'm');
+
+	//char message [8];
+
+	//snprintf (message, sizeof(message), "%.2f%  ", dutyCycle);
+	//RIT128x96x4StringDraw(message, 75, 80, 'm');
+	//IntMasterEnable();
+}
+
+void printTitles(float period){
+	RIT128x96x4StringDraw("Signal to Human", 20, 0, 'm');
+	RIT128x96x4StringDraw("Translation Interface", 0, 10, 'm');
+	RIT128x96x4StringDraw("Frequency:", 0, 60, 'm');
+	RIT128x96x4StringDraw("Period:", 0, 70, 'm');
 }
